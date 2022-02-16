6d1fb81fb44c4b685b119527b46081c67fbf4eb0
Mahsa Irankhah <m.irankhah1995@gmail.com>
2022-02-16 18:02:31 +0330

    complete/delete function and remove from local storage function

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let todoBtn = document.querySelector(".todo-button");
 let todoList = document.querySelector(".todo-list");
 
 todoBtn.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCompleteTodo)
 
 function addTodo(event) {
     event.preventDefault();
@@ -33,6 +34,19 @@ function addTodo(event) {
 }
 
 
+function deleteCompleteTodo (event) {
+    event.preventDefault();
+    let btn = event.target;
+    if (btn.classList[0] == "complete-btn") {
+        btn.parentElement.classList.toggle("completed")
+    }
+    if (btn.classList[0] == "trash-btn") {
+        btn.parentElement.remove();
+        removeFromLocal(btn.parentElement);
+    }
+}
+
+
 function addToLocalStorage (todo) {
     let todos;
     if (localStorage.getItem("todos") === null) {
@@ -42,4 +56,17 @@ function addToLocalStorage (todo) {
     }
     todos.push(todo);
     localStorage.setItem("todos", JSON.stringify(todos))
+}
+
+function removeFromLocal (todo) {
+    let todos;
+    if (localStorage.getItem("todos") === null) {
+      todos = [];
+    } else {
+      todos = JSON.parse(localStorage.getItem("todos"));
+    }
+    let todoText = todo.children[0].innerText;
+    console.log(todoText)
+    todos.splice(todos.indexOf(todoText), 1);
+    localStorage.setItem("todos", JSON.stringify(todos));
 }
