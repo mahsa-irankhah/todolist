0afcd497b882025692a3547a154a56b2c5378556
Mahsa Irankhah <m.irankhah1995@gmail.com>
2022-02-16 18:52:25 +0330

    completed the functionality

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 let todoInput = document.querySelector(".todo-input");
 let todoBtn = document.querySelector(".todo-button");
 let todoList = document.querySelector(".todo-list");
+let filterOptions = document.querySelector(".filter-todo");
 
 todoBtn.addEventListener("click", addTodo);
-todoList.addEventListener("click", deleteCompleteTodo)
+todoList.addEventListener("click", deleteCompleteTodo);
+filterOptions.addEventListener("click", selectOption);
+document.addEventListener("DOMContentLoaded", getTodos)
 
 function addTodo(event) {
     event.preventDefault();
@@ -66,7 +69,65 @@ function removeFromLocal (todo) {
       todos = JSON.parse(localStorage.getItem("todos"));
     }
     let todoText = todo.children[0].innerText;
-    console.log(todoText)
     todos.splice(todos.indexOf(todoText), 1);
     localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function getTodos () {
+    let todos;
+    if (localStorage.getItem("todos") === null) {
+      todos = [];
+    } else {
+      todos = JSON.parse(localStorage.getItem("todos"));
+    }
+    todos.forEach(function (todo) {
+        let todoDiv = document.createElement("div");
+        let todoItem = document.createElement("li");
+        let completeBtn = document.createElement("button");
+        let trashBtn = document.createElement("button");
+
+        todoItem.innerText = todo;
+        completeBtn.innerHTML = `<i class= "fas fa-check"></i>`;
+        trashBtn.innerHTML = `<i class= "fas fa-trash"></i>`;
+
+        todoDiv.classList.add("todo");
+        todoItem.classList.add("todo-item");
+        completeBtn.classList.add("complete-btn");
+        trashBtn.classList.add("trash-btn");
+
+        todoDiv.appendChild(todoItem);
+        todoDiv.appendChild(completeBtn);
+        todoDiv.appendChild(trashBtn);
+        todoList.appendChild(todoDiv);
+    })
+    
+}
+
+
+function selectOption(event) {
+    event.preventDefault();
+    let todos = todoList.childNodes;
+    let optionValue = event.target.value;
+    todos.forEach(function (todo) {
+       switch (optionValue) {
+         case "all":
+           todo.style.display = "flex";
+           break;
+         case "completed":
+           if (todo.classList.contains("completed")) {
+             todo.style.display = "flex";
+           } else {
+             todo.style.display = "none";
+           }
+           break;
+         case "uncompleted":
+           if (todo.classList.contains("completed")) {
+             todo.style.display = "none";
+           } else {
+             todo.style.display = "flex";
+           }
+           break;
+       }
+    })
+    
 }
